e458ac48e52d9e98cda6999dd293e1774324cb06
Magdy Saleh <magdy@predibase.com>
2023-08-25 11:02:18 -0400

    add tests

--- server/text_generation_server/utils/__init__.py
+++ server/text_generation_server/utils/__init__.py
@@ -2,6 +2,11 @@ from text_generation_server.utils.adapter import compute_delta_weight, create_me
 from text_generation_server.utils.convert import convert_file, convert_files
 from text_generation_server.utils.dist import initialize_torch_distributed
 from text_generation_server.utils.weights import Weights, get_start_stop_idxs_for_rank
+from text_generation_server.utils.s3 import (
+    weight_s3_files,
+    download_weights_from_s3,
+    weight_files_s3
+)
 from text_generation_server.utils.hub import (
     weight_files,
     weight_hub_files,
@@ -30,6 +35,9 @@ __all__ = [
     "weight_files",
     "weight_hub_files",
     "download_weights",
+    "weight_s3_files",
+    "download_weights_from_s3",
+    "weight_files_s3",
     "EntryNotFoundError",
     "HeterogeneousNextTokenChooser",
     "LocalEntryNotFoundError",
--- server/text_generation_server/utils/s3.py
+++ server/text_generation_server/utils/s3.py
@@ -37,6 +37,7 @@ def _get_local_cache_path():
 
 
 def try_to_load_from_cache(model_id: str, filename):
+    breakpoint()
     cache = _get_local_cache_path()
     model_path = cache / model_id
     if not model_path.is_dir():
@@ -46,42 +47,43 @@ def try_to_load_from_cache(model_id: str, filename):
 
 
 def weight_s3_files(
-    model_id: str, revision: Optional[str] = None, extension: str = ".safetensors"
+    model_id: str, extension: str = ".safetensors"
 ) -> List[str]:
     """Get the weights filenames from s3"""
-    bucket = _get_bucket_resource()   
+    bucket = _get_bucket_resource()
     model_files = bucket.objects.filter(Prefix=model_id)
-    filenames = [f.key for f in model_files if f.key.endswith(extension)]
+    filenames = [f.key.removeprefix(model_id).lstrip("/") for f in model_files if f.key.endswith(extension)]
     if not filenames:
         raise EntryNotFoundError(
-            f"No {extension} weights found for model {model_id} and revision {revision}.",
+            f"No {extension} weights found for model {model_id}",
             None,
         )
     return filenames
 
 
-def download_weights(
-    filenames: List[str], model_id: str, revision: Optional[str] = None
+def download_weights_from_s3(
+    filenames: List[str], model_id: str
 ) -> List[Path]:
     """Download the safetensors files from the s3"""
-    bucket = _get_bucket_resource()
     def download_file(filename, tries=5, backoff: int = 5):
-        local_file = try_to_load_from_cache(model_id, revision, filename)
+        local_file = try_to_load_from_cache(model_id, filename)
         if local_file is not None:
             logger.info(f"File {filename} already present in cache.")
             return Path(local_file)
-        # TODO: add cache support
         for i in range(tries):
             try:
                 logger.info(f"Download file: {filename}")
                 start_time = time.time()
                 local_file_path = _get_local_cache_path() / filename
+                # ensure cache dir exists and create it if needed
+                local_file_path.parent.mkdir(parents=True, exist_ok=True)
                 # TODO: add support for revision
-                local_file = bucket.download_file(filename, str(local_file_path))
                 logger.info(
-                    f"Downloaded {local_file} in {timedelta(seconds=int(time.time() - start_time))}."
+                    f"Downloaded {local_file_path} in {timedelta(seconds=int(time.time() - start_time))}."
                 )
-                return Path(local_file)
+                if not local_file_path.is_file():
+                    raise FileNotFoundError(f"File {local_file_path} not found")
+                return local_file_path
             except Exception as e:
                 if i + 1 == tries:
                     raise e
@@ -102,8 +104,8 @@ def download_weights(
     return files
 
 
-def weight_files(
-    model_id: str, revision: Optional[str] = None, extension: str = ".safetensors"
+def weight_files_s3(
+    model_id: str, extension: str = ".safetensors"
 ) -> List[Path]:
     """Get the local files"""
     # Local model
@@ -116,12 +118,12 @@ def weight_files(
         return local_files
 
     try:
-        filenames = weight_s3_files(model_id, revision, extension)
+        filenames = weight_s3_files(model_id, extension)
     except EntryNotFoundError as e:
         if extension != ".safetensors":
             raise e
         # Try to see if there are pytorch weights
-        pt_filenames = weight_s3_files(model_id, revision, extension=".bin")
+        pt_filenames = weight_s3_files(model_id, extension=".bin")
         # Change pytorch extension to safetensors extension
         # It is possible that we have safetensors weights locally even though they are not on the
         # hub if we converted weights locally without pushing them
@@ -129,5 +131,39 @@ def weight_files(
             f"{Path(f).stem.lstrip('pytorch_')}.safetensors" for f in pt_filenames
         ]
 
-    # TODO: add support for WEIGHTS_CACHE_OVERRIDE
-    return [Path(f) for f in filenames]
+    files = []
+    for filename in filenames:
+        cache_file = try_to_load_from_cache(
+            model_id, filename=filename
+        )
+        if cache_file is None:
+            raise LocalEntryNotFoundError(
+                f"File {filename} of model {model_id} not found in "
+                f"{_get_local_cache_path()}. "
+                f"Please run `text-generation-server download-weights {model_id}` first."
+            )
+        files.append(cache_file)
+
+    return files
+
+
+def main():
+    # test s3 files
+    filenames = weight_s3_files("opt-350m", extension=".bin")
+    assert filenames == ["pytorch_model.bin"]
+
+    # test errors
+    try:
+        weight_s3_files("opt-350m", extension=".errors")
+    except EntryNotFoundError as e:
+        print("Got correct error")
+
+    # test downloads
+    model_id = "opt-350m"
+    filenames = weight_s3_files(model_id, extension=".bin")
+    files = download_weights_from_s3(filenames, model_id)
+    local_files = weight_files_s3("opt-350m", extension=".bin")
+    assert files == local_files
+
+if __name__ == "__main__":
+    main()
