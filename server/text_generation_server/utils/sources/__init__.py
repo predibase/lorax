5ad66ae3f41567e5d1e205ff847c2705b2a35cc4
Magdy Saleh <magdy@predibase.com>
2023-08-31 14:58:16 +0000

    add consts

--- server/text_generation_server/utils/sources/__init__.py
+++ server/text_generation_server/utils/sources/__init__.py
@@ -1,11 +1,14 @@
 from .hub import EntryNotFoundError, LocalEntryNotFoundError, RevisionNotFoundError, weight_files, download_weights, weight_hub_files, HubModelSource
 from .s3 import S3ModelSource
 
+S3 = "s3"
+HUB = "hub"
+
 
 def get_model_source(source, model_id, revision, extension):
-    if source == "hub":
+    if source == HUB:
         return HubModelSource(model_id, revision, extension)
-    elif source == "s3":
+    elif source == S3:
         return S3ModelSource(model_id, revision, extension)
     else:
         raise ValueError(f"Unknown source {source}")
