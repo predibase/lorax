1794957ab9c54f9a179b6cc0aaa8dc6b9f1c4b98
Travis Addair <tgaddair@gmail.com>
2023-11-26 14:09:32 -0800

    Updated sgmv function

--- server/lorax_server/utils/sgmv.py
+++ server/lorax_server/utils/sgmv.py
@@ -10,27 +10,27 @@ def add_lora_sgmv_cutlass(
     x: torch.Tensor,
     wa_ptr: torch.Tensor,
     wb_ptr: torch.Tensor,
-    s: torch.IntTensor,
+    s_start: torch.IntTensor,
+    s_end: torch.IntTensor,
     layer_idx: int,
     lora_rank: int,
 ):
     """
     Semantics:
-    y[s[i]:s[i+1]] += x[s[i]:s[i+1]] @ deref(wa_ptr[i]) @ deref(wb_ptr[i])
-
+        y[s[i]:s[i+1]] += x[s[i]:s[i+1]] @ deref(wa_ptr[i]) @ deref(wb_ptr[i])
     Args:
-    y: Shape: `[B, H2]`. Output vectors. Will be changed in-place.
-    x: Shape: `[B, H1]`. Input vectors.
-    wa_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
-        Weight matrix shape: `[num_layers, H1, R]`.
-    wb_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
-        Weight matrix shape: `[num_layers, R, H2]`.
-    s: Shape: `[S+1]`, DType: torch.int32. Indptr of the weight matrices.\
-        `s[0] == 0`, `s[-1] == B`.
-    layer_idx: Layer index of the weight matrices.
+        y: Shape: `[B, H2]`. Output vectors. Will be changed in-place.
+        x: Shape: `[B, H1]`. Input vectors.
+        wa_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
+            Weight matrix shape: `[num_layers, H1, R]`.
+        wb_ptr: Shape: `[S]`. DType: torch.int64. Pointer to the weight matrices.\
+            Weight matrix shape: `[num_layers, R, H2]`.
+        s_start: Shape: `[S]`, DType: torch.int32. Indptr of the weight matrices start indices.
+        s_end: Shape: `[S]`, DType: torch.int32. Indptr of the weight matrices end indices.
+        layer_idx: Layer index of the weight matrices.
     """
     tmp_size = _kernels.sgmv_cutlass_tmp_size(wa_ptr.size(0))
     tmp = torch.empty((tmp_size,), dtype=torch.uint8, device=x.device)
     v = torch.zeros((x.size(0), lora_rank), dtype=x.dtype, device=x.device)
-    _kernels.sgmv_cutlass(v, x, wa_ptr, s, tmp, layer_idx)
-    _kernels.sgmv_cutlass(y, v, wb_ptr, s, tmp, layer_idx)
+    _kernels.sgmv_cutlass(v, x, wa_ptr, s_start, s_end, tmp, layer_idx)
+    _kernels.sgmv_cutlass(y, v, wb_ptr, s_start, s_end, tmp, layer_idx)
