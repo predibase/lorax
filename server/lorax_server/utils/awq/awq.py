9a21bd55e40da2e9c6a7ec9cbb04af0773bbdc93
flozi00 <you@example.com>
2023-12-05 12:51:30 +0100

    ttf awq kernel call

--- server/lorax_server/utils/awq/awq.py
+++ server/lorax_server/utils/awq/awq.py
@@ -1,53 +1,32 @@
-# Copied logic from https://github.com/mit-han-lab/llm-awq/blob/19a5a2c9db47f69a2851c83fea90f81ed49269ab/awq/quantize/qmodule.py
+# Copied logic from https://github.com/mit-han-lab/llm-awq/blob/f084f40bd996f3cf3a0633c1ad7d9d476c318aaa/awq/quantize/qmodule.py
 
 import math
 import torch
 import torch.nn as nn
 import awq_inference_engine  # with CUDA kernels
 
-def make_divisible(c, divisor):
-    return (c + divisor - 1) // divisor
-
-
-def calculate_zeros_width(in_features, group_size=128, pack_num=8):
-    if group_size >= 128:
-        size_multiplier = 1
-    elif group_size == 64:
-        size_multiplier = 2
-    elif group_size == 32:
-        size_multiplier = 4
-    else:
-        raise NotImplementedError
-    
-    base_width = make_divisible(in_features // group_size, pack_num)
-    base_width = make_divisible(base_width, size_multiplier) * size_multiplier
-    return base_width
-
-
 class AWQLinear(nn.Module):
-    def __init__(self, w_bit, group_size, in_features, out_features, bias, dev):
+    def __init__(self, w_bit, group_size, qweight, qzeros, scales, bias):
         super().__init__()
-        
-        if w_bit not in [4]:
+
+        if w_bit != 4:
             raise NotImplementedError("Only 4-bit are supported for now.")
-        
-        self.in_features = in_features
-        self.out_features = out_features
-        self.w_bit = w_bit
-        self.group_size = group_size if group_size != -1 else in_features
+
+        self.in_features = qweight.shape[0]
+        self.out_features = qweight.shape[1] * 32 // w_bit
+
         self.split_k_iters = 8
-        # quick sanity check (make sure aligment)
-        assert self.in_features % self.group_size == 0
-        assert out_features % (32 // self.w_bit) == 0
-        pack_num = (32 // self.w_bit)
-        # TODO (Haotian): a function for buffer shape calculation
-        self.register_buffer('qweight', torch.zeros((out_features, in_features // pack_num), dtype=torch.int32, device=dev))
-        self.register_buffer('qzeros', torch.zeros((out_features, calculate_zeros_width(in_features, self.group_size)), dtype=torch.int32, device=dev))
-        self.register_buffer('scales', torch.zeros((out_features, calculate_zeros_width(in_features, self.group_size) * pack_num), dtype=torch.float16, device=dev))
-        if bias:
-            self.register_buffer('bias', torch.zeros((out_features), dtype=torch.float16, device=dev))
-        else:
-            self.bias = None
+
+        self.w_bit = w_bit
+        self.group_size = group_size if group_size != -1 else self.in_features
+
+        assert self.in_features % self.group_size == 0, "in_features must be divisible by group_size"
+        assert self.out_features % (32 // self.w_bit) == 0, "out_features must be divisible by 32 // w_bit"
+
+        self.qweight = qweight
+        self.qzeros = qzeros
+        self.scales = scales
+        self.bias = bias if bias else None
 
     @torch.no_grad()
     def forward(self, x):
@@ -58,8 +37,6 @@ class AWQLinear(nn.Module):
         else:
             out = awq_inference_engine.gemv_forward_cuda(inputs, self.qweight, self.scales, self.qzeros, self.group_size)
         out = out + self.bias if self.bias is not None else out
-        #print(out)
-        #assert 0
         return out.reshape(out_shape)
     
     @property
