ab4c6a7b66665e42e012f6890613d164dc0b3c26
Travis Addair <tgaddair@gmail.com>
2023-12-14 22:22:26 -0800

    Fixed license

--- server/lorax_server/models/custom_modeling/flash_phi_modeling.py
+++ server/lorax_server/models/custom_modeling/flash_phi_modeling.py
@@ -1,38 +1,13 @@
 # Adapted from
-# https://huggingface.co/microsoft/phi-1_5/blob/main/modeling_phi.py
+# https://huggingface.co/microsoft/phi-2/blob/main/modeling_phi.py
+# and
+# https://github.com/vllm-project/vllm/blob/main/vllm/model_executor/models/phi_1_5.py
+#
+# Copyright 2023 Predibase.
 # Copyright 2023 The vLLM team.
 # Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
 #
-# BSD 3-Clause License
-#
-# Copyright (c) 2022, Tri Dao, trid@cs.stanford.edu.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-#
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-#
-# * Neither the name of the copyright holder nor the names of its
-#   contributors may be used to endorse or promote products derived from
-#   this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# LICENSE: https://huggingface.co/microsoft/phi-2/blob/main/LICENSE
 
 import torch
 import torch.distributed
