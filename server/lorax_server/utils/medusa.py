60c837b724fce84f63721e8c319b96c4f6c9abb1
flozi00 <you@example.com>
2023-12-18 19:36:41 +0100

    expand medusa

--- server/lorax_server/utils/medusa.py
+++ server/lorax_server/utils/medusa.py
@@ -24,12 +24,12 @@ class ResBlock(torch.nn.Module):
 class MedusaModel(torch.nn.Module):
     def __init__(self, config, weights, lm_head):
         super().__init__()
-        self.heads = torch.nn.ModuleList(
-            [
-                MedusaHead(config, prefix=f"{i}", weights=weights)
-                for i in range(config["medusa_num_heads"])
-            ]
-        )
+
+        self.heads = torch.nn.ModuleList()
+        for i in range(config["medusa_num_heads"]):
+            head = MedusaHead(config, prefix=f"{i}", weights=weights)
+            self.heads.append(head)
+
         self.lm_head = lm_head
 
     def forward(self, x):
@@ -41,12 +41,12 @@ class MedusaModel(torch.nn.Module):
 class MedusaHead(torch.nn.Module):
     def __init__(self, config, prefix, weights):
         super().__init__()
-        self.blocks = torch.nn.ModuleList(
-            [
-                ResBlock(config, prefix=f"{prefix}.{i}", weights=weights)
-                for i in range(config["medusa_num_layers"])
-            ]
-        )
+
+        self.blocks = torch.nn.ModuleList()
+        for i in range(config["medusa_num_layers"]):
+            block = ResBlock(config, prefix=f"{prefix}.{i}", weights=weights)
+            self.blocks.append(block)
+
         n = len(self.blocks)
         self.out = FastLinear.load(
             config, prefix=f"{prefix}.{n}", weights=weights, bias=False
