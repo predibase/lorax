b7f2f922e29cddd3b79bf06a9ef88c3f47a6230f
Adam Kelch <askelch@gmail.com>
2024-01-23 12:55:33 -0500

    change temperature validation to include 0

--- clients/python/lorax/types.py
+++ clients/python/lorax/types.py
@@ -88,7 +88,7 @@ class Parameters(BaseModel):
 
     @validator("temperature")
     def valid_temp(cls, v):
-        if v is not None and v <= 0:
+        if v is not None and v < 0:
             raise ValidationError("`temperature` must be strictly positive")
         return v
 
