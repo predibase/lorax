db8a824c7e2cfb2df2c11bf577bc9cfca6a1bb26
Magdy Saleh <magdy@predibase.com>
2024-02-21 16:02:35 -0500

    bump

--- clients/python/lorax/__init__.py
+++ clients/python/lorax/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 from lorax.client import Client, AsyncClient, MergedAdapters
