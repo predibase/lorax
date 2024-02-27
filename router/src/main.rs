74f0c2857406d634015b04c5256c40c47df56885
Travis Addair <tgaddair@gmail.com>
2024-02-27 15:10:07 -0800

    Update tokenizers to v0.15 to be consistent with server (#285)

--- router/src/main.rs
+++ router/src/main.rs
@@ -1,6 +1,8 @@
 /// LoRAX webserver entrypoint
 use axum::http::{HeaderName, HeaderValue};
 use clap::Parser;
+use hf_hub::api::tokio::{Api, ApiBuilder, ApiRepo};
+use hf_hub::{Repo, RepoType};
 use lorax_client::{ClientError, ShardedClient};
 use lorax_router::{server, HubModelInfo};
 use opentelemetry::sdk::propagation::TraceContextPropagator;
@@ -9,11 +11,13 @@ use opentelemetry::sdk::trace::Sampler;
 use opentelemetry::sdk::Resource;
 use opentelemetry::{global, KeyValue};
 use opentelemetry_otlp::WithExportConfig;
+use std::fs::File;
+use std::io::BufReader;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::path::Path;
 use std::time::Duration;
 use thiserror::Error;
-use tokenizers::{FromPretrainedParameters, Tokenizer};
+use tokenizers::tokenizer::Tokenizer;
 use tower_http::cors::{AllowCredentials, AllowHeaders, AllowMethods, AllowOrigin, ExposeHeaders};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -81,7 +85,8 @@ struct Args {
     adapter_source: String,
 }
 
-fn main() -> Result<(), RouterError> {
+#[tokio::main]
+async fn main() -> Result<(), RouterError> {
     // Get args
     let args = Args::parse();
     // Pattern match configuration
@@ -115,6 +120,8 @@ fn main() -> Result<(), RouterError> {
         ngrok_edge,
         adapter_source,
     } = args;
+
+    init_logging(otlp_endpoint, json_output);
 
     // Validate args
     if max_input_length >= max_total_tokens {
@@ -191,159 +198,179 @@ fn main() -> Result<(), RouterError> {
     // Parse Huggingface hub token
     let authorization_token = std::env::var("HUGGING_FACE_HUB_TOKEN").ok();
 
-    // Tokenizer instance
-    // This will only be used to validate payloads
+    // Load tokenizer
+    tracing::info!("Loading tokenizer {tokenizer_name}");
     let local_path = Path::new(&tokenizer_name);
     let local_model = local_path.exists() && local_path.is_dir();
     let tokenizer = if local_model {
-        // Load local tokenizer
+        tracing::info!("Using local tokenizer: {0}", local_path.to_str().unwrap());
         Tokenizer::from_file(local_path.join("tokenizer.json")).ok()
     } else {
-        // Download and instantiate tokenizer
-        // We need to download it outside of the Tokio runtime
-        let params = FromPretrainedParameters {
-            revision: revision.clone().unwrap_or("main".to_string()),
-            auth_token: authorization_token.clone(),
-            ..Default::default()
-        };
-        Tokenizer::from_pretrained(tokenizer_name.clone(), Some(params)).ok()
-    };
+        // Shared API builder initialization
+        let api_builder = || {
+            let mut builder = ApiBuilder::new()
+                .with_progress(false)
+                .with_token(authorization_token.clone());
 
-    // Launch Tokio runtime
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()?
-        .block_on(async {
-            init_logging(otlp_endpoint, json_output);
-
-            if tokenizer.is_none() {
-                tracing::warn!(
-                    "Could not find a fast tokenizer implementation for {tokenizer_name}"
-                );
-                tracing::warn!("Rust input length validation and truncation is disabled");
+            if let Ok(cache_dir) = std::env::var("HUGGINGFACE_HUB_CACHE") {
+                builder = builder.with_cache_dir(cache_dir.into());
             }
 
-            // Get Model info
-            let model_info = match local_model {
-                true => HubModelInfo {
-                    model_id: tokenizer_name.clone(),
+            builder
+        };
+
+        tracing::info!("Using the Hugging Face API");
+        let api_opt = match api_builder().build() {
+            Ok(api) => Some(api),
+            Err(_) => {
+                tracing::warn!("Unable to build the Hugging Face API");
+                None
+            }
+        };
+
+        if api_opt.is_none() {
+            return Err(RouterError::ArgumentValidation(
+                "Unable to build the Hugging Face API".to_string(),
+            ));
+        }
+
+        let api = api_opt.unwrap();
+        let api_repo = api.repo(Repo::with_revision(
+            tokenizer_name.to_string(),
+            RepoType::Model,
+            revision.clone().unwrap_or_else(|| "main".to_string()),
+        ));
+
+        match api_repo.get("tokenizer.json").await {
+            Ok(tokenizer_filename) => Tokenizer::from_file(tokenizer_filename).ok(),
+            Err(_) => get_base_tokenizer(&api, &api_repo).await,
+        }
+    };
+
+    if tokenizer.is_none() {
+        tracing::warn!("Could not find a fast tokenizer implementation for {tokenizer_name}");
+        tracing::warn!("Rust input length validation and truncation is disabled");
+    }
+
+    // Get Model info
+    let model_info = match local_model {
+        true => HubModelInfo {
+            model_id: tokenizer_name.clone(),
+            sha: None,
+            pipeline_tag: None,
+        },
+        false => get_model_info(&tokenizer_name, revision, authorization_token)
+            .await
+            .unwrap_or_else(|| {
+                tracing::warn!("Could not retrieve model info from the Hugging Face hub.");
+                HubModelInfo {
+                    model_id: tokenizer_name.to_string(),
                     sha: None,
                     pipeline_tag: None,
-                },
-                false => get_model_info(&tokenizer_name, revision, authorization_token)
-                    .await
-                    .unwrap_or_else(|| {
-                        tracing::warn!("Could not retrieve model info from the Hugging Face hub.");
-                        HubModelInfo {
-                            model_id: tokenizer_name.to_string(),
-                            sha: None,
-                            pipeline_tag: None,
-                        }
-                    }),
-            };
-
-            // if pipeline-tag == lorax we default to return_full_text = true
-            let compat_return_full_text = match &model_info.pipeline_tag {
-                None => {
-                    tracing::warn!("no pipeline tag found for model {tokenizer_name}");
-                    false
                 }
-                Some(pipeline_tag) => pipeline_tag.as_str() == "lorax",
-            };
+            }),
+    };
 
-            // Instantiate sharded client from the master unix socket
-            let mut sharded_client = ShardedClient::connect_uds(master_shard_uds_path)
-                .await
-                .map_err(RouterError::Connection)?;
-            // Clear the cache; useful if the webserver rebooted
-            sharded_client
-                .clear_cache(None)
-                .await
-                .map_err(RouterError::Cache)?;
-            // Get info from the shard
-            let shard_info = sharded_client.info().await.map_err(RouterError::Info)?;
+    // if pipeline-tag == lorax we default to return_full_text = true
+    let compat_return_full_text = match &model_info.pipeline_tag {
+        None => {
+            tracing::warn!("no pipeline tag found for model {tokenizer_name}");
+            false
+        }
+        Some(pipeline_tag) => pipeline_tag.as_str() == "lorax",
+    };
 
-            // Warmup model
-            tracing::info!("Warming up model");
-            let max_supported_batch_total_tokens = match sharded_client
-                .warmup(max_input_length as u32, max_batch_prefill_tokens)
-                .await
-                .map_err(RouterError::Warmup)?
-            {
-                // Older models do not support automatic max-batch-total-tokens
-                None => {
-                    let max_batch_total_tokens = max_batch_total_tokens.unwrap_or(
-                        16000.max((max_total_tokens as u32).max(max_batch_prefill_tokens)),
+    // Instantiate sharded client from the master unix socket
+    let mut sharded_client = ShardedClient::connect_uds(master_shard_uds_path)
+        .await
+        .map_err(RouterError::Connection)?;
+    // Clear the cache; useful if the webserver rebooted
+    sharded_client
+        .clear_cache(None)
+        .await
+        .map_err(RouterError::Cache)?;
+    // Get info from the shard
+    let shard_info = sharded_client.info().await.map_err(RouterError::Info)?;
+
+    // Warmup model
+    tracing::info!("Warming up model");
+    let max_supported_batch_total_tokens = match sharded_client
+        .warmup(max_input_length as u32, max_batch_prefill_tokens)
+        .await
+        .map_err(RouterError::Warmup)?
+    {
+        // Older models do not support automatic max-batch-total-tokens
+        None => {
+            let max_batch_total_tokens = max_batch_total_tokens
+                .unwrap_or(16000.max((max_total_tokens as u32).max(max_batch_prefill_tokens)));
+            tracing::warn!("Model does not support automatic max batch total tokens");
+            max_batch_total_tokens
+        }
+        // Flash attention models return their max supported total tokens
+        Some(max_supported_batch_total_tokens) => {
+            // Warn if user added his own max-batch-total-tokens as we will ignore it
+            let mut max_effective_batch_total_tokens = max_supported_batch_total_tokens;
+            if max_batch_total_tokens.is_some() {
+                // Check if manual value is lower than inferred value
+                let max_batch_total_tokens = max_batch_total_tokens.unwrap();
+                if max_batch_total_tokens < max_supported_batch_total_tokens {
+                    max_effective_batch_total_tokens = max_batch_total_tokens;
+                } else {
+                    tracing::warn!(
+                        "`--max-batch-total-tokens` is deprecated for Flash \
+                    Attention models."
                     );
-                    tracing::warn!("Model does not support automatic max batch total tokens");
-                    max_batch_total_tokens
+                    tracing::warn!(
+                        "Inferred max batch total tokens: {max_supported_batch_total_tokens}"
+                    );
                 }
-                // Flash attention models return their max supported total tokens
-                Some(max_supported_batch_total_tokens) => {
-                    // Warn if user added his own max-batch-total-tokens as we will ignore it
-                    let mut max_effective_batch_total_tokens = max_supported_batch_total_tokens;
-                    if max_batch_total_tokens.is_some() {
-                        // Check if manual value is lower than inferred value
-                        let max_batch_total_tokens = max_batch_total_tokens.unwrap();
-                        if max_batch_total_tokens < max_supported_batch_total_tokens {
-                            max_effective_batch_total_tokens = max_batch_total_tokens;
-                        } else {
-                            tracing::warn!(
-                                "`--max-batch-total-tokens` is deprecated for Flash \
-                            Attention models."
-                            );
-                            tracing::warn!(
-                                "Inferred max batch total tokens: {max_supported_batch_total_tokens}"
-                            );
-                        }
-                    }
-                    max_effective_batch_total_tokens
-                }
-            };
-            tracing::info!("Setting max batch total tokens to {max_supported_batch_total_tokens}");
-            tracing::info!("Connected");
+            }
+            max_effective_batch_total_tokens
+        }
+    };
+    tracing::info!("Setting max batch total tokens to {max_supported_batch_total_tokens}");
+    tracing::info!("Connected");
 
-            let addr = match hostname.parse() {
-                Ok(ip) => SocketAddr::new(ip, port),
-                Err(_) => {
-                    tracing::warn!("Invalid hostname, defaulting to 0.0.0.0");
-                    SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port)
-                }
-            };
+    let addr = match hostname.parse() {
+        Ok(ip) => SocketAddr::new(ip, port),
+        Err(_) => {
+            tracing::warn!("Invalid hostname, defaulting to 0.0.0.0");
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port)
+        }
+    };
 
-            // Run server
-            server::run(
-                model_info,
-                shard_info,
-                compat_return_full_text,
-                max_concurrent_requests,
-                max_best_of,
-                max_stop_sequences,
-                max_input_length,
-                max_total_tokens,
-                waiting_served_ratio,
-                max_batch_prefill_tokens,
-                max_supported_batch_total_tokens,
-                max_waiting_tokens,
-                max_active_adapters,
-                adapter_cycle_time_s,
-                sharded_client,
-                tokenizer,
-                validation_workers,
-                addr,
-                cors_allow_origin,
-                cors_allow_method,
-                cors_allow_credentials,
-                cors_allow_header,
-                cors_expose_header,
-                ngrok,
-                ngrok_authtoken,
-                ngrok_edge,
-                adapter_source,
-            )
-            .await?;
-            Ok(())
-        })
+    // Run server
+    server::run(
+        model_info,
+        shard_info,
+        compat_return_full_text,
+        max_concurrent_requests,
+        max_best_of,
+        max_stop_sequences,
+        max_input_length,
+        max_total_tokens,
+        waiting_served_ratio,
+        max_batch_prefill_tokens,
+        max_supported_batch_total_tokens,
+        max_waiting_tokens,
+        max_active_adapters,
+        adapter_cycle_time_s,
+        sharded_client,
+        tokenizer,
+        validation_workers,
+        addr,
+        cors_allow_origin,
+        cors_allow_method,
+        cors_allow_credentials,
+        cors_allow_header,
+        cors_expose_header,
+        ngrok,
+        ngrok_authtoken,
+        ngrok_edge,
+        adapter_source,
+    )
+    .await?;
+    Ok(())
 }
 
 /// Init logging using env variables LOG_LEVEL and LOG_FORMAT:
@@ -437,6 +464,31 @@ pub async fn get_model_info(
             );
         }
         Some(hub_model_info)
+    } else {
+        None
+    }
+}
+
+/// get base tokenizer
+pub async fn get_base_tokenizer(api: &Api, api_repo: &ApiRepo) -> Option<Tokenizer> {
+    let config_filename = api_repo.get("config.json").await.ok()?;
+
+    // Open the file in read-only mode with buffer.
+    let file = File::open(config_filename).ok()?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file as an instance of `User`.
+    let config: serde_json::Value = serde_json::from_reader(reader).ok()?;
+
+    if let Some(serde_json::Value::String(base_model_id)) = config.get("base_model_name_or_path") {
+        let api_base_repo = api.repo(Repo::with_revision(
+            base_model_id.to_string(),
+            RepoType::Model,
+            "main".to_string(),
+        ));
+
+        let tokenizer_filename = api_base_repo.get("tokenizer.json").await.ok()?;
+        Tokenizer::from_file(tokenizer_filename).ok()
     } else {
         None
     }
