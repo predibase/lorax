80f1f2b3b948713038276eab90b6a103cffa53be
Travis Addair <tgaddair@gmail.com>
2024-02-27 16:34:57 -0800

    cargo fmt

--- router/client/src/client.rs
+++ router/client/src/client.rs
@@ -150,7 +150,11 @@ impl Client {
         };
 
         let max_new_tokens = max_total_tokens - max_input_length;
-        let request = tonic::Request::new(WarmupRequest { batch: Some(batch), max_new_tokens }).inject_context();
+        let request = tonic::Request::new(WarmupRequest {
+            batch: Some(batch),
+            max_new_tokens,
+        })
+        .inject_context();
         let response = self.stub.warmup(request).await?.into_inner();
         Ok(response.max_supported_total_tokens)
     }
