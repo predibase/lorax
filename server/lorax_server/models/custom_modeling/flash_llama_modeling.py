77a7e54e36fac43e97678720d7b475243b67a71c
Travis Addair <tgaddair@gmail.com>
2024-03-13 14:59:47 -0700

    Plumbing

--- server/lorax_server/models/custom_modeling/flash_llama_modeling.py
+++ server/lorax_server/models/custom_modeling/flash_llama_modeling.py
@@ -527,6 +527,7 @@ class FlashLlamaForCausalLM(torch.nn.Module):
         input_lengths: torch.Tensor,
         max_s: int,
         adapter_data: AdapterBatchData,
+        prefill_cache_indices: Optional[torch.Tensor] = None,
         lm_head_indices: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         hidden_states = self.model(
