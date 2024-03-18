5c07ad8b16c8b58aa9b5ca999d1a1c53d628a10b
Travis Addair <tgaddair@gmail.com>
2024-03-18 14:41:51 -0700

    Fix server

--- server/lorax_server/models/flash_causal_lm.py
+++ server/lorax_server/models/flash_causal_lm.py
@@ -924,6 +924,8 @@ class FlashCausalLM(Model):
             slots=slots,
             input_lengths=input_lengths,
             max_s=max_s,
+            adapter_data=adapter_data,
+            prefill_cache_indices=batch.prefill_cache_indices,
             lm_head_indices=batch.prefill_head_indices,
         )
         if batch.prefill_cache_indices is not None:
@@ -1129,7 +1131,7 @@ class FlashCausalLM(Model):
                 seed,
                 num_accepted_ids,
         ) in enumerate(iterator):
-            all_alternative_tokens = [] request.parameters.return_k_alternatives > 0 else None
+            all_alternative_tokens = [] if request.parameters.return_k_alternatives > 0 else None
             next_token_texts = []
             left = 0
             current_stopped = False
