3f312260108cec5763b2d348db3f65c1ee7f6cdb
Travis Addair <tgaddair@gmail.com>
2024-03-18 14:29:43 -0700

    Cleanup

--- router/src/infer.rs
+++ router/src/infer.rs
@@ -611,9 +611,9 @@ fn send_responses(
         next_tokens.texts, 
         next_tokens.is_special,
         alternative_tokens,
-    )).enumerate().peekable();
+    )).peekable();
     
-    while let Some((i, (id, logprob, text, special, alternative_tokens))) = iterator.next() {
+    while let Some((id, logprob, text, special, alternative_tokens)) = iterator.next() {
         let token = Token {
             id,
             text,
@@ -630,83 +630,32 @@ fn send_responses(
                 )
             }),
         };
-        let top_tokens = if let Some(top_tokens_) = generation.top_tokens.get(i) {
-            top_tokens_
-                .ids
-                .iter()
-                .zip(top_tokens_.logprobs.iter())
-                .zip(top_tokens_.texts.iter())
-                .zip(top_tokens_.is_special.iter())
-                .map(|(((&id, &logprob), text), &special)| Token {
-                    id,
-                    text: text.to_string(),
-                    logprob,
-                    special,
-                })
-                .collect()
-        } else {
-            vec![]
-        };
 
         match (&generation.generated_text, iterator.peek()) {
             (Some(generated_text), None) => {
                 // Generation has ended
                 stopped = true;
                 // Send message
-                entry.response_tx.send(Ok(InferStreamResponse::End {
-                    token,
-                    top_tokens,
-                    generated_text: generated_text.clone(),
-                    queued: entry.queue_time,
-                    start: entry.batch_time.unwrap(),
-                }))?;
+                entry.response_tx.send_timeout(
+                    Ok(InferStreamResponse::End {
+                        token,
+                        generated_text: generated_text.clone(),
+                        queued: entry.queue_time,
+                        start: entry.batch_time.unwrap(),
+                    }),
+                    Duration::from_millis(10),
+                )?;
             }
             _ => {
                 // Send message
-                entry
-                    .response_tx
-                    .send(Ok(InferStreamResponse::Intermediate { token, top_tokens }))?;
+                entry.response_tx.send_timeout(
+                    Ok(InferStreamResponse::Token(token)),
+                    Duration::from_millis(10),
+                )?;
             }
         }
     }
 
-    let token = Token {
-        id: generation.token_id,
-        text: generation.token_text,
-        logprob: generation.token_logprob,
-        special: generation.token_is_special,
-        alternative_tokens: generation.alternative_tokens.and_then(|at| {
-            Some(
-                at.ids
-                    .into_iter()
-                    .zip(at.logprobs.into_iter())
-                    .zip(at.texts.into_iter())
-                    .map(|((id, logprob), text)| AlternativeToken { id, text, logprob })
-                    .collect(),
-            )
-        }),
-    };
-
-    if let Some(generated_text) = generation.generated_text {
-        // Generation has ended
-        stopped = true;
-        // Send message
-        entry.response_tx.send_timeout(
-            Ok(InferStreamResponse::End {
-                token,
-                generated_text,
-                queued: entry.queue_time,
-                start: entry.batch_time.unwrap(),
-            }),
-            Duration::from_millis(10),
-        )?;
-    } else {
-        // Send message
-        entry.response_tx.send_timeout(
-            Ok(InferStreamResponse::Token(token)),
-            Duration::from_millis(10),
-        )?;
-    }
     Ok(stopped)
 }
 
