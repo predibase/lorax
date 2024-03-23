934922d1710bb3f8dd656e55ff1bd3941a7071bd
Travis Addair <tgaddair@gmail.com>
2024-03-22 17:15:42 -0700

    Upgrade python client to v0.4.0

--- clients/python/lorax/__init__.py
+++ clients/python/lorax/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 from lorax.client import Client, AsyncClient, MergedAdapters
