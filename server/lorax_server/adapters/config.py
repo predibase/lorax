4b710b16adb1b844460d96c38c55b4a05a3f9ab2
Travis Addair <tgaddair@gmail.com>
2024-03-28 10:00:11 -0700

    Config

--- server/lorax_server/adapters/config.py
+++ server/lorax_server/adapters/config.py
@@ -32,22 +32,3 @@ class AdapterConfig(ABC):
         unused_weight_names: Set[str],
     ) -> Optional[AdapterWeights]:
         pass
-
-
-@dataclass
-class MedusaConfig(AdapterConfig):
-    medusa_num_heads: int
-    medusa_num_layers: int
-
-    def map_weights_for_model(
-        self, adapter_weights: Dict, weight_names: Tuple[str],
-    ) -> Tuple[ModuleMap, Set[str]]:
-        return adapter_weights, set(weight_names)
-
-    @classmethod
-    def load(cls, config: dict) -> "MedusaConfig":
-        return cls(
-            base_model_name_or_path=config["base_model_name_or_path"],
-            medusa_num_heads=config["medusa_num_heads"],
-            medusa_num_layers=config["medusa_num_layers"],
-        )
