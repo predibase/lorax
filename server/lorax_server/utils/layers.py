08e6d262ca770466dc115db8464fdbf68f0a025f
Travis Addair <tgaddair@gmail.com>
2024-04-16 15:23:06 -0700

    Try TGI implementation

--- server/lorax_server/utils/layers.py
+++ server/lorax_server/utils/layers.py
@@ -316,12 +316,54 @@ class Linear4bit(nn.Module):
         return out
 
 
-from float8_experimental.float8_linear_utils import (
-    swap_linear_with_float8_linear,
-)
-from float8_experimental.float8_dynamic_linear import Float8DynamicLinear
+# from float8_experimental.float8_linear_utils import (
+#     swap_linear_with_float8_linear,
+# )
+# from float8_experimental.float8_dynamic_linear import Float8DynamicLinear
 
 # from float8_experimental.float8_linear import Float8Linear
+
+
+def fp8_quantize(weight, qdtype=torch.float8_e4m3fn):
+    device = weight.device
+    # weight, scale = quant_weights(weight, torch.int8, False)
+    finfo = torch.finfo(qdtype)
+    # Calculate the scale as dtype max divided by absmax
+    scale = finfo.max / weight.abs().max().clamp(min=1e-12)
+    # scale and clamp the tensor to bring it to
+    # the representative range of float8 data type
+    # (as default cast is unsaturated)
+    qweight = (weight * scale).clamp(min=finfo.min, max=finfo.max)
+    # Return both float8 data and the inverse scale (as float),
+    # as both required as inputs to torch._scaled_mm
+    qweight = qweight.to(qdtype)
+    scale = scale.float().reciprocal()
+    return qweight, scale
+
+
+class Fp8Linear(nn.Module):
+    def __init__(
+        self,
+        weight,
+        bias,
+    ) -> None:
+        super().__init__()
+        self.dtype = weight.dtype
+        self.qweight, self.scale = fp8_quantize(weight)
+
+        self.bias = bias if bias is not None else None
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        qinput, scale = fp8_quantize(input)
+        output, _ = torch._scaled_mm(
+            qinput,
+            self.qweight.t(),
+            out_dtype=self.dtype,
+            scale_a=scale,
+            scale_b=self.scale,
+            bias=self.bias,
+        )
+        return output
 
 
 def get_linear(weight, bias, quantize, fan_in_fan_out=False):
@@ -331,10 +373,12 @@ def get_linear(weight, bias, quantize, fan_in_fan_out=False):
         weight = weight.T.contiguous()
 
     if True:
-        linear = FastLinear(weight, bias)
-        linear.in_features = weight.shape[1]
-        linear.out_features = weight.shape[0]
-        linear = Float8DynamicLinear.from_float(linear)
+        # linear = FastLinear(weight, bias)
+        # linear.in_features = weight.shape[1]
+        # linear.out_features = weight.shape[0]
+        # linear = Float8DynamicLinear.from_float(linear)
+
+        linear = Fp8Linear(weight, bias)
     elif quantize is None:
         linear = FastLinear(weight, bias)
     elif quantize == "bitsandbytes":
