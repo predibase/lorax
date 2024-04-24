6bb07dba49853faef73979de4795ba0cbe30b38e
Travis Addair <tgaddair@gmail.com>
2024-04-24 09:23:36 -0700

    Python client v0.5.0

--- clients/python/lorax/__init__.py
+++ clients/python/lorax/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from lorax.client import Client, AsyncClient, MergedAdapters
