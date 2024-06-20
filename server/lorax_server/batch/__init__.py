7b23cac4db958e360f3f5678319c1969bfcc56aa
Travis Addair <travis@predibase.com>
2024-06-20 15:17:14 -0700

    WIP: filtering

--- server/lorax_server/batch/__init__.py
+++ server/lorax_server/batch/__init__.py
@@ -1,11 +1,23 @@
+from collections import defaultdict
+from dataclasses import dataclass
 import time
-from typing import List, Optional
+from typing import Dict, List, Optional, Tuple
 import grpc
 from datasets import load_dataset
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
 from lorax_server.pb import generate_pb2, generate_pb2_grpc
+
+
+BLOCK_SIZE = 16
+_ID = 0
+
+
+@dataclass
+class Entry:
+    request: generate_pb2.Request
+    input_length: int
 
 
 def run(
@@ -21,16 +33,20 @@ def run(
 ):
     t0 = time.time()
     with grpc.insecure_channel(f"unix://{uds_path}") as channel:
-        stub = generate_pb2_grpc.LoraxServiceStub(channel)
+        client = generate_pb2_grpc.LoraxServiceStub(channel)
 
         # health check to ensure system is up
-        resp = stub.Health(generate_pb2.HealthRequest())
+        resp = client.Health(generate_pb2.HealthRequest())
         print("HEALTH RESPONSE", resp, type(resp))
+
+        # get deployment info
+        info = client.Info(generate_pb2.InfoRequest())
+        window_size = info.window_size
 
         # warmup
         # TODO(travis): set warmup constraints based on input data
         max_supported_total_tokens = warmup(
-            stub=stub,
+            client=client,
             max_input_length=max_input_length,
             max_batch_prefill_tokens=max_batch_prefill_tokens,
             max_total_tokens=max_total_tokens,
@@ -50,40 +66,37 @@ def run(
         )
         print(tokenized_dataset[0])
 
-        # Convert dataset to requests
-        requests = []
+        # Convert dataset to entries
+        entries = []
         for i, example in enumerate(tokenized_dataset):
                 text = example["text"]
                 input_ids = example["input_ids"]
-                request = create_request(
+                entry = create_entry(
                     inputs=text,
                     input_length=len(input_ids),
                     max_input_length=max_input_length,
                     max_total_tokens=max_total_tokens,
                 )
-                requests.append(request)
-
+                entries.append(entry)
+        
         # continuous batching
-        cached_batch = None
+        outputs = defaultdict(list)
         token_budget = max_supported_total_tokens
         with tqdm(total=len(tokenized_dataset)) as pbar:
-            while requests:
-                request = requests.pop(0)
-            
-
-                prefill_tokens += ((entry.request.input_length() + self.block_size - 1)
-                    / self.block_size)
-                    * self.block_size;
-
-                batch = generate_pb2.Batch(
-                    id=i,
-                    size=1,
-                    requests=[request],
-                    max_tokens=0,
-                )
-
+            while batch := next_batch(entries, max_batch_prefill_tokens, token_budget, BLOCK_SIZE, window_size):
                 # prefill
-                    
+                cached_batch, generations = prefill(client, batch)
+                batches = [cached_batch]
+                add_outputs(generations, outputs)
+
+                while new_batch := next_batch(entries, max_batch_prefill_tokens, token_budget, BLOCK_SIZE, window_size):
+                    new_cached_batch = prefill(client, new_batch)
+                    batches.append(new_cached_batch)
+                    add_outputs(generations, outputs)
+
+                # decode
+                cached_batch, generations = decode(client, batches)
+                add_outputs(generations, outputs)
 
                 pbar.update(1)
 
@@ -93,36 +106,16 @@ def run(
     print("BATCH RUN COMPLETE", time.time() - t0)
 
 
-def next_batch(
-    requests: List[generate_pb2.Request],
-    max_batch_prefill_tokens: int,
-    token_budget: int,
-) -> Optional[generate_pb2.Batch]:
-    batch_requests = []
-    prefill_tokens = 0
-    decode_tokens = 0
-    while requests and max_batch_tokens <= token_budget:
-        request = requests.pop(0)
-
-        # pad to block size
-        prefill_tokens += ((request.input_length + self.block_size - 1)
-            / self.block_size)
-            * self.block_size;
-
-
-        batch_requests.append(request)
-        max_batch_tokens += request.stopping_parameters.max_new_tokens
-    
-    return generate_pb2.Batch(
-        id=0,
-        size=len(batch_requests),
-        requests=batch_requests,
-        max_tokens=max_batch_tokens,
-    )
+def add_outputs(
+    generations: List[generate_pb2.Generation],
+    outputs: Dict[int, List[str]],
+):
+    for generation in generations:
+        outputs[generation.request_id].append(generation.generated_text.text)
 
 
 def warmup(
-    stub: generate_pb2_grpc.LoraxServiceStub,
+    client: generate_pb2_grpc.LoraxServiceStub,
     max_input_length: int,
     max_batch_prefill_tokens: int,
     max_total_tokens: int,
@@ -174,20 +167,47 @@ def warmup(
         batch=batch,
         max_new_tokens=max_new_tokens,
     )
-    resp = stub.Warmup(request)
+    resp = client.Warmup(request)
 
     return resp.max_supported_total_tokens
 
 
-def create_request(
+def prefill(
+    client: generate_pb2_grpc.LoraxServiceStub,
+    batch: generate_pb2.Batch,
+) -> Tuple[generate_pb2.CachedBatch, List[generate_pb2.Generation]]:
+    resp = client.Prefill(batch)
+    return resp.batch, resp.generations
+
+
+def decode(
+    client: generate_pb2_grpc.LoraxServiceStub,
+    batches: List[generate_pb2.CachedBatch],
+) -> Tuple[generate_pb2.CachedBatch, List[generate_pb2.Generation]]:
+    resp = client.Decode(batches)
+    return resp.batch, resp.generations
+
+
+def filter_batch(
+    client: generate_pb2_grpc.LoraxServiceStub,
+    next_batch: Optional[generate_pb2.CachedBatch],
+) -> Optional[generate_pb2.CachedBatch]:
+    if next_batch is None:
+        return None
+
+    resp = client.Filter(next_batch)
+    return resp.batch
+
+
+def create_entry(
     inputs: str,
     input_length: int,
     max_input_length: int,
     max_total_tokens: int,
-) -> generate_pb2.Request:
+) -> Entry:
     # We truncate the input on the server side to be sure that it has the correct size
     effective_max_new_tokens = max_total_tokens - input_length
-    return generate_pb2.Request(
+    request = generate_pb2.Request(
         id=0,
         inputs=inputs,
         truncate=max_input_length,
@@ -199,7 +219,7 @@ def create_request(
             typical_p=1,
             do_sample=False,
             seed=0,
-            repetition_penalty=1.2,
+            repetition_penalty=1,
             watermark=False,
             adapter_id="",
             schema=None,
@@ -213,4 +233,58 @@ def create_request(
         adapter_index=0,
         prefill_logprobs=True,
         apply_chat_template=False,
+    )
+
+    return Entry(
+        request=request,
+        input_length=input_length,
+    )
+
+
+def next_batch(
+    entries: List[Entry],
+    max_batch_prefill_tokens: int,
+    token_budget: int,
+    block_size: int,
+    window_size: Optional[int],
+) -> Optional[generate_pb2.Batch]:
+    batch_requests = []
+    prefill_tokens = 0
+    decode_tokens = 0
+    while entries and (prefill_tokens + decode_tokens) <= token_budget:
+        entry = entries.pop(0)
+
+        # update prefill tokens
+        prefill_tokens += ((entry.input_length + block_size - 1) / block_size) * block_size
+
+        # update decode tokens
+        if window_size is None:
+            max_new_tokens = entry.request.stopping_parameters.max_new_tokens
+        else:
+            max_new_tokens = min(
+                window_size - entry.input_length,
+                entry.request.stopping_parameters.max_new_tokens,
+            )
+        decode_tokens += ((max_new_tokens + block_size - 1) / block_size) * block_size
+
+        if prefill_tokens > max_batch_prefill_tokens or (prefill_tokens + decode_tokens) > token_budget:
+            # Entry is over budget
+            # Add it back to the front
+            entries.insert(0, entry)
+            break
+
+        batch_requests.append(entry.request)
+    
+    if not batch_requests:
+        return None
+    
+    global _ID
+    next_id = _ID
+    _ID += 1
+
+    return generate_pb2.Batch(
+        id=next_id,
+        size=len(batch_requests),
+        requests=batch_requests,
+        max_tokens=prefill_tokens + decode_tokens,
     )
