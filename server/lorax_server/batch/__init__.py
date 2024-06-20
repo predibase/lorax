c1a0a0f03520aef46766538b290bd75b0bf9a2eb
Travis Addair <travis@predibase.com>
2024-06-20 14:11:29 -0700

    WIP: continuous batching

--- server/lorax_server/batch/__init__.py
+++ server/lorax_server/batch/__init__.py
@@ -1,4 +1,5 @@
 import time
+from typing import List, Optional
 import grpc
 from datasets import load_dataset
 from tqdm import tqdm
@@ -49,15 +50,75 @@ def run(
         )
         print(tokenized_dataset[0])
 
+        # Convert dataset to requests
+        requests = []
+        for i, example in enumerate(tokenized_dataset):
+                text = example["text"]
+                input_ids = example["input_ids"]
+                request = create_request(
+                    inputs=text,
+                    input_length=len(input_ids),
+                    max_input_length=max_input_length,
+                    max_total_tokens=max_total_tokens,
+                )
+                requests.append(request)
+
         # continuous batching
+        cached_batch = None
+        token_budget = max_supported_total_tokens
         with tqdm(total=len(tokenized_dataset)) as pbar:
-            for i, example in enumerate(tokenized_dataset):
+            while requests:
+                request = requests.pop(0)
+            
+
+                prefill_tokens += ((entry.request.input_length() + self.block_size - 1)
+                    / self.block_size)
+                    * self.block_size;
+
+                batch = generate_pb2.Batch(
+                    id=i,
+                    size=1,
+                    requests=[request],
+                    max_tokens=0,
+                )
+
+                # prefill
+                    
+
                 pbar.update(1)
 
         # stream out the output parquet file
         # TODO(travis) explore streaing writing: https://stackoverflow.com/questions/64791558/create-parquet-files-from-stream-in-python-in-memory-efficient-manner
     
     print("BATCH RUN COMPLETE", time.time() - t0)
+
+
+def next_batch(
+    requests: List[generate_pb2.Request],
+    max_batch_prefill_tokens: int,
+    token_budget: int,
+) -> Optional[generate_pb2.Batch]:
+    batch_requests = []
+    prefill_tokens = 0
+    decode_tokens = 0
+    while requests and max_batch_tokens <= token_budget:
+        request = requests.pop(0)
+
+        # pad to block size
+        prefill_tokens += ((request.input_length + self.block_size - 1)
+            / self.block_size)
+            * self.block_size;
+
+
+        batch_requests.append(request)
+        max_batch_tokens += request.stopping_parameters.max_new_tokens
+    
+    return generate_pb2.Batch(
+        id=0,
+        size=len(batch_requests),
+        requests=batch_requests,
+        max_tokens=max_batch_tokens,
+    )
 
 
 def warmup(
@@ -71,11 +132,11 @@ def warmup(
 
     while n_tokens < max_batch_prefill_tokens:
         # We truncate the input on the server side to be sure that it has the correct size
-        truncate_length = min(max_input_length, max_batch_prefill_tokens - n_tokens);
+        truncate_length = min(max_input_length, max_batch_prefill_tokens - n_tokens)
         requests.append(generate_pb2.Request(
             id=0,
             inputs="_test " * max_input_length,
-            truncate=truncate_length,
+            truncate=max_input_length,
             # Set sampling parameters to also take these ops into account in the max memory
             parameters=generate_pb2.NextTokenChooserParameters(
                 temperature=0.9,
@@ -116,3 +177,40 @@ def warmup(
     resp = stub.Warmup(request)
 
     return resp.max_supported_total_tokens
+
+
+def create_request(
+    inputs: str,
+    input_length: int,
+    max_input_length: int,
+    max_total_tokens: int,
+) -> generate_pb2.Request:
+    # We truncate the input on the server side to be sure that it has the correct size
+    effective_max_new_tokens = max_total_tokens - input_length
+    return generate_pb2.Request(
+        id=0,
+        inputs=inputs,
+        truncate=max_input_length,
+        # Set sampling parameters to also take these ops into account in the max memory
+        parameters=generate_pb2.NextTokenChooserParameters(
+            temperature=1,
+            top_k=0,
+            top_p=1,
+            typical_p=1,
+            do_sample=False,
+            seed=0,
+            repetition_penalty=1.2,
+            watermark=False,
+            adapter_id="",
+            schema=None,
+            return_k_alternatives=0,
+        ),
+        stopping_parameters=generate_pb2.StoppingCriteriaParameters(
+            max_new_tokens=effective_max_new_tokens,
+            stop_sequences=[],
+            ignore_eos_token=False,
+        ),
+        adapter_index=0,
+        prefill_logprobs=True,
+        apply_chat_template=False,
+    )
