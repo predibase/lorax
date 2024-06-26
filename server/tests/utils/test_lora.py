63019b95f5b3fd4e3524bb89ab61e7e5e4084268
Ajinkya Tejankar <tejankarajinkya@gmail.com>
2024-06-26 15:07:28 -0700

    tests: expand test cases to check for correct adapter pointers

--- server/tests/utils/test_lora.py
+++ server/tests/utils/test_lora.py
@@ -9,6 +9,7 @@ from lorax_server.adapters.lora import LoraWeights
 from lorax_server.adapters.types import LORA
 from lorax_server.adapters.weights import AdapterBatchMetadata, AdapterWeights, BatchAdapterWeights, LayerAdapterWeights
 from lorax_server.utils.sgmv import MIN_RANK_CUSTOM
+from lorax_server.utils.segments import find_segments
 
 
 class FakeAdapterWeights(AdapterWeights):
@@ -43,16 +44,37 @@ class FakeBatchAdapterWeights(BatchAdapterWeights):
 
 
 @pytest.mark.parametrize(
-    "lora_ranks",
+    "lora_ranks,adapter_indices,expected",
     [
-        [8, 16],
-        [32, 64],
+        (
+            [8, 8, 16], # ranks of adapters
+            [0, 0, 1, 1, 0, 0, 1, 1, 2, 2], # adapter indices for each token in the batch
+            {
+                8: ( # rank
+                    [0, 2, 4, 6], # expected segment starts
+                    [2, 4, 6, 8], # expected segment ends
+                    [0, 1, 0, 1], # expected adapter indices
+                ),
+                16: ([8], [10], [2]),
+            }
+        ),
+        (
+            [4, 8, 16],
+            [0, 0, 1, 1, 0, 0, 1, 1, 2, 2],
+            {
+                4: ([0, 4], [2, 6], [0, 0]),
+                8: ([2, 6], [4, 8], [1, 1]),
+                16: ([8], [10], [2]),
+            }
+        ),
     ],
 )
-def test_batched_lora_weights(lora_ranks: List[int]):
-    # batch meta is hardcoded with this assumption below
-    assert len(lora_ranks) == 2
-
+def test_batched_lora_weights(
+    lora_ranks: List[int],
+    adapter_indices: List[int],
+    expected: Dict[int, Tuple[List[int], Tuple[int], Tuple[int]]]
+):
+    num_adapters = len(lora_ranks)
     batched_weights = LayerAdapterWeights()
     assert batched_weights.is_empty()
 
@@ -69,39 +91,50 @@ def test_batched_lora_weights(lora_ranks: List[int]):
         batched_weights.add_adapter(idx, weights)
 
     assert not batched_weights.is_empty()
-    assert len(batched_weights.adapter_weights) == 2
+    assert len(batched_weights.adapter_weights) == num_adapters
+
+    segments, segment_indices = find_segments(adapter_indices)
 
     meta = AdapterBatchMetadata(
-        adapter_indices=torch.tensor([0, 0, 1, 1, 0, 0, 1, 1], dtype=torch.int64),
-        adapter_set={0, 1},
-        adapter_segments=torch.tensor([0, 2, 4, 6, 8], dtype=torch.int64),
-        segment_indices=[0, 1, 0, 1],
+        adapter_indices=torch.tensor(adapter_indices, dtype=torch.int64),
+        adapter_set=set(adapter_indices),
+        adapter_segments=torch.tensor(segments, dtype=torch.int64),
+        segment_indices=segment_indices,
     )
 
     with mock.patch("lorax_server.adapters.lora.get_tmp_tensors", return_value=(torch.empty(0), torch.empty(0))):
         data = batched_weights.get_data(meta, prefill=True, prefill_head_indices=None, is_embed=False).get(LORA)
 
-    assert len(data.lora_a) == 2
+    assert len(data.lora_a) == num_adapters
+    assert len(data.lora_b) == num_adapters
     assert data.lora_a.keys() == meta.adapter_set
-    assert data.lora_a[0].shape == ((1, h, lora_ranks[0]) if lora_ranks[0] < MIN_RANK_CUSTOM else (1, lora_ranks[0], h))
-    assert data.lora_a[1].shape == ((1, h, lora_ranks[1]) if lora_ranks[1] < MIN_RANK_CUSTOM else (1, lora_ranks[1], h))
-
-    assert len(data.lora_b) == 2
     assert data.lora_b.keys() == meta.adapter_set
-    assert data.lora_b[0].shape == (1, lora_ranks[0], h)
-    assert data.lora_b[1].shape == (1, lora_ranks[1], h)
+    for i in range(num_adapters):
+        assert data.lora_a[i].shape == (
+            (1, h, lora_ranks[i]) if lora_ranks[i] < MIN_RANK_CUSTOM else (1, lora_ranks[i], h)
+        )
+        assert data.lora_b[i].shape == (1, lora_ranks[i], h)
 
-    assert len(data.rank_data) == 2
-    assert data.rank_data.keys() == set(lora_ranks)
     for lora_rank, rd in data.rank_data.items():
         assert rd.rank == lora_rank
+        expected_lora_a_ptr = []
+        expected_lora_b_ptr = []
+        for adapter_idx in expected[lora_rank][2]:
+            expected_lora_a_ptr.append(batched_weights.adapter_weights[adapter_idx].weights_a.data_ptr())
+            expected_lora_b_ptr.append(batched_weights.adapter_weights[adapter_idx].weights_b.data_ptr())
+        expected_lora_a_ptr = torch.tensor(expected_lora_a_ptr, dtype=rd.lora_a_ptr.dtype, device=rd.lora_a_ptr.device)
+        expected_lora_b_ptr = torch.tensor(expected_lora_b_ptr, dtype=rd.lora_b_ptr.dtype, device=rd.lora_b_ptr.device)
+        assert all(rd.lora_a_ptr == expected_lora_a_ptr)
+        assert all(rd.lora_b_ptr == expected_lora_b_ptr)
 
-        # shape in all cases is the number of segments with this rank
-        assert rd.lora_a_ptr.shape == (2,)
-        assert rd.lora_b_ptr.shape == (2,)
-        assert rd.segment_starts.shape == (2,)
-        assert rd.segment_ends.shape == (2,)
-
+        expected_segment_starts = torch.tensor(
+            expected[lora_rank][0], dtype=rd.segment_starts.dtype, device=rd.segment_starts.device
+        )
+        expected_segment_ends = torch.tensor(
+            expected[lora_rank][1], dtype=rd.segment_ends.dtype, device=rd.segment_ends.device
+        )
+        assert all(rd.segment_ends == expected_segment_ends)
+        assert all(rd.segment_starts == expected_segment_starts)
 
 
 @pytest.mark.parametrize(
@@ -111,18 +144,20 @@ def test_batched_lora_weights(lora_ranks: List[int]):
             [8, 8, 16], # ranks of adapters
             [0, 0, 1, 1, 0, 0, 1, 1, 2, 2], # adapter indices for each token in the batch
             {
-                # rank: (expected pointer tensor size, expected indices tensor in the rank data)
-                8: (2, [0, 0, 1, 1, 0, 0, 1, 1, -1, -1]),
-                16: (1, [-1, -1, -1, -1, -1, -1, -1, -1, 0, 0])
+                8: ( # rank
+                    [0, 1], # expected adapter indices
+                    [0, 0, 1, 1, 0, 0, 1, 1, -1, -1] # expected indices
+                ),
+                16: ([2], [-1, -1, -1, -1, -1, -1, -1, -1, 0, 0]),
             }
         ),
         (
             [4, 8, 16],
             [0, 0, 1, 1, 0, 0, 1, 1, 2, 2],
             {
-                4: (1, [0, 0, -1, -1, 0, 0, -1, -1, -1, -1]),
-                8: (1, [-1, -1, 0, 0, -1, -1, 0, 0, -1, -1]),
-                16: (1, [-1, -1, -1, -1, -1, -1, -1, -1, 0, 0]),
+                4: ([0], [0, 0, -1, -1, 0, 0, -1, -1, -1, -1]),
+                8: ([1], [-1, -1, 0, 0, -1, -1, 0, 0, -1, -1]),
+                16: ([2], [-1, -1, -1, -1, -1, -1, -1, -1, 0, 0]),
             }
         ),
     ],
@@ -130,14 +165,12 @@ def test_batched_lora_weights(lora_ranks: List[int]):
 def test_batched_lora_weights_decode(
     lora_ranks: List[int],
     adapter_indices: List[int],
-    expected: Dict[int, Tuple[int, List[int]]]
+    expected: Dict[int, Tuple[List[int], List[int]]]
 ):
-    from lorax_server.utils.segments import find_segments
     batched_weights = LayerAdapterWeights()
     assert batched_weights.is_empty()
 
     h = 1024
-    adapter_weights = []
     for idx, lora_rank in enumerate(lora_ranks):
         weights = LoraWeights(
             weights_a=[torch.randn((h, lora_rank), dtype=torch.float16)],
@@ -159,10 +192,19 @@ def test_batched_lora_weights_decode(
         data = batched_weights.get_data(meta, prefill=False, prefill_head_indices=None, is_embed=False).get(LORA)
 
     for lora_rank, rd in data.rank_data.items():
+        expected_lora_a_ptr = []
+        expected_lora_b_ptr = []
+        for adapter_idx in expected[lora_rank][0]:
+            expected_lora_a_ptr.append(batched_weights.adapter_weights[adapter_idx].weights_a_t.data_ptr())
+            expected_lora_b_ptr.append(batched_weights.adapter_weights[adapter_idx].weights_b_t.data_ptr())
+        expected_lora_a_ptr = torch.tensor(expected_lora_a_ptr, dtype=rd.lora_a_ptr.dtype, device=rd.lora_a_ptr.device)
+        expected_lora_b_ptr = torch.tensor(expected_lora_b_ptr, dtype=rd.lora_b_ptr.dtype, device=rd.lora_b_ptr.device)
+        assert all(rd.lora_a_ptr == expected_lora_a_ptr)
+        assert all(rd.lora_b_ptr == expected_lora_b_ptr)
+
         expected_indices = torch.tensor(expected[lora_rank][1], dtype=rd.indices.dtype, device=rd.indices.device)
-        assert rd.lora_a_ptr.shape == (expected[lora_rank][0],)
-        assert rd.lora_b_ptr.shape == (expected[lora_rank][0],)
         assert all(rd.indices == expected_indices)
+
         assert rd.segment_starts == None
         assert rd.segment_ends == None
         assert rd.tmp_shrink == None
