38c91727959913ebac230eea81db978a507b3a2a
Arnav Garg <arnav@predibase.com>
2024-07-01 15:58:08 -0700

    handle fp32 as fp16

--- server/lorax_server/models/__init__.py
+++ server/lorax_server/models/__init__.py
@@ -76,12 +76,10 @@ def get_model(
     if dtype is None:
         dtype = config_dict.get("torch_dtype", "float16")
 
-    if dtype == "float16":
+    if dtype == "float16" or dtype == "float32":
         dtype = torch.float16
     elif dtype == "bfloat16":
         dtype = torch.bfloat16
-    elif dtype == "float32":
-        dtype = torch.float32
     else:
         try:
             dtype = getattr(torch, dtype)
