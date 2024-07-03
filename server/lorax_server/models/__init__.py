24cb49440606469dc489d0267fdfeb9223f5db43
Arnav Garg <106701836+arnavgarg1@users.noreply.github.com>
2024-07-03 17:48:50 -0500

    Infer dtype from model config when not explicitly specified (#534)

--- server/lorax_server/models/__init__.py
+++ server/lorax_server/models/__init__.py
@@ -15,6 +15,7 @@ from lorax_server.models.santacoder import SantaCoder
 from lorax_server.models.seq2seq_lm import Seq2SeqLM
 from lorax_server.models.t5 import T5Sharded
 from lorax_server.utils.sources import get_s3_model_local_dir
+from lorax_server.utils.torch_utils import is_bf16_supported
 
 # The flag below controls whether to allow TF32 on matmul. This flag defaults to False
 # in PyTorch 1.12 and later.
@@ -72,15 +73,24 @@ def get_model(
         raise ValueError(f"Unknown source {source}")
 
     model_type = config_dict["model_type"]
+    is_dtype_provided = dtype is not None
+    dtype = dtype or config_dict.get("torch_dtype", "float16")
 
-    if dtype is None:
-        dtype = torch.float16
-    elif dtype == "float16":
+    if dtype in {"float16", "float32"}:
         dtype = torch.float16
     elif dtype == "bfloat16":
-        dtype = torch.bfloat16
+        if not is_bf16_supported():
+            if is_dtype_provided:
+                raise RuntimeError("bfloat16 is not supported on this device, set --dtype float16.")
+            logger.warning("bfloat16 is not supported on this device, falling back to float16")
+            dtype = torch.float16
+        else:
+            dtype = torch.bfloat16
     else:
-        raise RuntimeError(f"Unknown dtype {dtype}")
+        try:
+            dtype = getattr(torch, dtype)
+        except AttributeError:
+            raise RuntimeError(f"Unknown dtype {dtype}")
 
     if "facebook/galactica" in model_id:
         return GalacticaSharded(
