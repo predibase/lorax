dfad64b325de5f411af893315ec20deefaa4f994
Noah Yoshida <noahcy117@gmail.com>
2024-08-10 10:54:18 -0700

    init tracing otel

--- router/src/main.rs
+++ router/src/main.rs
@@ -508,7 +508,7 @@ fn init_logging(otlp_endpoint: Option<String>, json_output: bool) {
 
         if let Ok(tracer) = tracer {
             layers.push(tracing_opentelemetry::layer().with_tracer(tracer).boxed());
-            axum_tracing_opentelemetry::init_propagator().unwrap();
+            init_tracing_opentelemetry::tracing_subscriber_ext.init_subscribers()?;
         };
     }
 
