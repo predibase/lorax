3650d3836f3f1275a1796791f0f7dbcb8490a6bf
Magdy Saleh <magdy@predibase.com>
2024-11-08 12:53:35 +0000

    manual trigger and fix test

--- integration-tests-2/test_embeddings.py
+++ integration-tests-2/test_embeddings.py
@@ -9,7 +9,7 @@ def test_stella_1_5b():
         "docker_args": {"embedding_dim": 256},
     }
     with run_lorax_container(config):
-        response = requests.post("http://localhost:8080/embed", json={"inputs": ["Hello, world!"]})
+        response = requests.post("http://localhost:8080/embed", json={"inputs": "Hello, world!"})
         response.raise_for_status()
         print("RESPONSE FROM EMBEDDING: ", response.json())
         assert len(response.json()["embeddings"]) > 0
