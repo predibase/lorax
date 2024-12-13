9e288b48a096c9a23df09fcd72aa72079a8227c8
Magdy Saleh <magdy@predibase.com>
2024-12-13 17:28:27 -0500

    fixes

--- router/src/infer.rs
+++ router/src/infer.rs
@@ -501,10 +501,12 @@ impl Infer {
                 err
             })?;
 
+        let embed_params = request.parameters.unwrap_or_default();
+
         let (adapter_source, adapter_parameters) = extract_adapter_params(
-            request.parameters.adapter_id.clone(),
-            request.parameters.adapter_source.clone(),
-            request.parameters.adapter_parameters.clone(),
+            embed_params.adapter_id.clone(),
+            embed_params.adapter_source.clone(),
+            embed_params.adapter_parameters.clone(),
         );
 
         let adapter_idx;
@@ -520,7 +522,7 @@ impl Infer {
             }
         }
 
-        let api_token = request.parameters.api_token.clone();
+        let api_token = embed_params.api_token.clone();
         let adapter = Adapter::new(
             adapter_parameters,
             adapter_source.unwrap(),
@@ -875,10 +877,12 @@ impl Infer {
                 err
             })?;
 
+        let embed_params = request.parameters.clone().unwrap_or_default();
+
         let (adapter_source, adapter_parameters) = extract_adapter_params(
-            request.parameters.adapter_id.clone(),
-            request.parameters.adapter_source.clone(),
-            request.parameters.adapter_parameters.clone(),
+            embed_params.adapter_id.clone(),
+            embed_params.adapter_source.clone(),
+            embed_params.adapter_parameters.clone(),
         );
 
         let adapter_idx;
@@ -894,7 +898,7 @@ impl Infer {
             }
         }
 
-        let api_token = request.parameters.api_token.clone();
+        let api_token = embed_params.api_token.clone();
         let adapter = Adapter::new(
             adapter_parameters,
             adapter_source.unwrap(),
--- router/src/server.rs
+++ router/src/server.rs
@@ -486,12 +486,12 @@ async fn health(
     if health.shard_info().supports_embeddings {
         let embed_request = EmbedRequest {
             inputs: "San Francisco".to_string(),
-            parameters: EmbedParameters {
+            parameters: Some(EmbedParameters {
                 adapter_id: None,
                 adapter_source: None,
                 adapter_parameters: None,
                 api_token: None,
-            },
+            }),
         };
         match infer.embed(embed_request).await {
             Ok(_) => {}
