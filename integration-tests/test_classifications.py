69bb989df8357fdc65bd5d8ce9df687baf521950
Magdy Saleh <17618143+magdyksaleh@users.noreply.github.com>
2024-12-17 16:28:11 -0500

    fix and add test (#721)

--- integration-tests/test_classifications.py
+++ integration-tests/test_classifications.py
@@ -16,7 +16,33 @@ def test_distilbert_ner():
     with run_lorax_container(config):
         response = requests.post(
             "http://localhost:8080/classify",
-            json={"inputs": "Johnny supports the Golden State Warriors. He lives in London."},
+            json={
+                "inputs": "Johnny supports the Golden State Warriors. He lives in London."
+            },
+        )
+        response.raise_for_status()
+        print("RESPONSE FROM CLASSIFICATION:", response.json())
+        assert len(response.json()) > 0
+
+
+def test_bert_ner():
+    config = {
+        "name": "bert-ner",
+        "model_id": "magdyks/bert-base-ner",
+        "docker_args": {
+            "max_input_length": 512,
+            "max_batch_prefill_tokens": 512,
+            "max_batch_total_tokens": 512,
+            "max_total_tokens": 512,
+            "backend": "flashinfer",
+        },
+    }
+    with run_lorax_container(config):
+        response = requests.post(
+            "http://localhost:8080/classify",
+            json={
+                "inputs": "Johnny supports the Golden State Warriors. He lives in London."
+            },
         )
         response.raise_for_status()
         print("RESPONSE FROM CLASSIFICATION:", response.json())
