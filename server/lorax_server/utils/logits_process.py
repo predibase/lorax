4bacba6b79fcc603d7f9a79e48c0e5e06f650aa8
Jeff Kinnison <jeffery.kinnison@predibase.com>
2025-01-28 17:28:05 -0800

    Revert "reorganize imports"
    This reverts commit f26a6e663f74284949860e517614d9c8a206f5f8.

--- server/lorax_server/utils/logits_process.py
+++ server/lorax_server/utils/logits_process.py
@@ -4,8 +4,6 @@ from functools import lru_cache
 from typing import Dict, List, Optional, Union
 
 import torch
-from outlines.fsm.guide import RegexGuide
-from outlines.fsm.json_schema import build_regex_from_schema
 from transformers import (
     LogitsProcessor,
     LogitsWarper,
@@ -17,6 +15,8 @@ from transformers import (
 )
 
 # try:
+from outlines.fsm.guide import RegexGuide
+from outlines.fsm.json_schema import build_regex_from_schema
 
 HAS_OUTLINES = True
 # except ImportError:
